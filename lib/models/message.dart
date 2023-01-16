f8ea2c45c6f47aaa16365da5566a47530214a429
semihyagci <semiihyagci@gmail.com>
2023-01-17 01:57:34 +0300

    firebase v4

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,2 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Message{
+  String content;
+  String senderName;
+  String senderID;
+
+  Message(this.content, this.senderName, this.senderID);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'content': content,
+      'senderName': senderName,
+      'senderID': senderID,
+    };
+  }
+
+  Message.fromMap(Map<String, dynamic> messageMap)
+      : content = messageMap["content"],
+        senderName = messageMap["senderName"],
+        senderID = messageMap["senderID"];
+
+  Message.fromDocumentSnapshot(DocumentSnapshot<Map<String, dynamic>> doc)
+      : content = doc.data()!["content"],
+        senderName = doc.data()!["senderName"],
+        senderID =doc.data()!["senderID"];
+
 }
